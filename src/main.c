3f52b4462223f85f073f0a9bfb9b3e6f81f5d3cf
s1xc37 <csidleno2@gmail.com>
2026-06-20 03:32:05 +0300

    minor

--- src/main.c
+++ src/main.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int get_neighboors_qty(bool* point);
-void print_matrix(bool** matrix);
+int get_neighboors_qty(bool** data, size_t m, size_t n, int i_i, int j_i);
 bool** generate_matrix(size_t m, size_t n);
 void free_subptr(void** data);
+
 void field_input(bool** data, size_t m, size_t n);
 void field_output(bool** data, size_t m, size_t n);
+void field_update(bool** data, size_t m, size_t n);
 
 int main(void) {
     size_t n, m;
@@ -16,8 +17,10 @@ int main(void) {
     bool** matrix = generate_matrix(m, n);
     field_input(matrix, m, n);
     field_output(matrix, m, n);
-    free_subptr((void**)matrix);
-    free(matrix);
+    field_update(matrix, m, n);
+    field_output(matrix, m, n);
+    // free_subptr((void**)matrix);
+    // free(matrix);
     return 0;
 }
 
@@ -51,4 +54,29 @@ void field_output(bool** data, size_t m, size_t n) {
         }
         printf("\n");
     }
+}
+
+void field_update(bool** data, size_t m, size_t n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int neighboors_qty = get_neighboors_qty(data, m, n, i, j);
+            if(neighboors_qty >= 3 && data[i][j] == false) {
+                data[i][j] = true;
+            }
+            if(neighboors_qty < 2 && data[i][j] == true) {
+                data[i][j] = false;
+            }
+        }
+    }
+}
+
+int get_neighboors_qty(bool** data, size_t m, size_t n, int i_i, int j_i) {
+    int neighboors_qty = 0;
+    for(int i = i_i - 1; i <= i_i + 1; i++) {
+        for(int j = j_i - 1; i <= j_i + 1; j++) {
+            int i_norm = i % n, j_norm = j % m;
+            if(data[i_norm][j_norm] == 1) neighboors_qty++;
+        }
+    }
+    return neighboors_qty;
 }
